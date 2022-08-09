05aeec1cc89cb61b0a8339b69a4f617053ca1134
manami young <manayou1125@outlook.jp>
2022-08-09 18:12:14 +0900

    add error messages

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -7,5 +7,21 @@ class PropertiesController < ApplicationController
   def new
     @property = Property.new
   end
-  
+
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      redirect to properties_path, notice: '投稿しました！'
+    else
+      render :new
+    end
+  end
+
+
+
+
+  def property_params
+    params.require(:property).permit(:name, :rent, :address, :year, :content)
+  end
+
 end
